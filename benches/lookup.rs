d1f000a0fccbf27f45b208aa82b4b9949f8af8bf
Techcable <git@techcable.net>
2025-08-07 19:30:50 -0700

    Add basic lookup benchmark
    Based on the benchmark in the thread_local crate.
    Also benchmarks thread_local + Arc overhead.
    On my M1 Mac, Arc::clone dwarfs the overhead of all lookups.
    
    This is unfortunate, because Arc::clone is required for our crate.

---
+++ benches/lookup.rs
@@ -0,0 +1,50 @@
+#![allow(missing_docs)]
+use std::hint::black_box;
+use std::sync::Arc;
+
+use dropping_thread_local::DroppingThreadLocal;
+use thread_local::ThreadLocal;
+
+fn main() {
+    let mut c = criterion::Criterion::default().configure_from_args();
+
+    c.bench_function("std::get", |b| {
+        std::thread_local!(static LOCAL: Box<i32> = Box::new(0));
+        LOCAL.with(|x| black_box(**x));
+        b.iter(|| {
+            black_box(LOCAL.with(|x| **x));
+        });
+    });
+
+    c.bench_function("std::get_arc", |b| {
+        std::thread_local!(static LOCAL: Arc<i32> = Arc::new(0));
+        LOCAL.with(|x| black_box(**x));
+        b.iter_with_large_drop(|| {
+            black_box(LOCAL.with(|x| Arc::clone(x)));
+        });
+    });
+
+    c.bench_function("thread_local::get", |b| {
+        let local = ThreadLocal::new();
+        local.get_or(|| Box::new(0));
+        b.iter(|| {
+            black_box(local.get());
+        });
+    });
+
+    c.bench_function("thread_local::get_arc", |b| {
+        let local = ThreadLocal::new();
+        local.get_or(|| Arc::new(0));
+        b.iter_with_large_drop(|| {
+            black_box(Arc::clone(local.get().unwrap()));
+        });
+    });
+
+    c.bench_function("dropping_thread_local::get", |b| {
+        let local = DroppingThreadLocal::new();
+        local.get_or_init(|| Box::new(0));
+        b.iter_with_large_drop(|| {
+            black_box(local.get());
+        });
+    });
+}
